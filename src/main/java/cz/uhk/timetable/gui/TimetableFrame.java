cb17a6c669d72cc6848127226fd99ac6278bc370
Jakub Didkovský <jakub@didkovsky.cz>
2026-04-14 13:36:44 +0200

    GUI implementation, Gson dependency

---
+++ src/main/java/cz/uhk/timetable/gui/TimetableFrame.java
@@ -0,0 +1,100 @@
+package cz.uhk.timetable.gui;
+
+import cz.uhk.timetable.model.LocationTimetable;
+import cz.uhk.timetable.utils.TimetableProvider;
+import cz.uhk.timetable.utils.impl.MockTimetableProvider;
+
+import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import java.awt.*;
+
+public class TimetableFrame extends JFrame {
+        private LocationTimetable timetable;
+        private TimetableProvider provider = new MockTimetableProvider();
+        private JTable tabTimetable;
+        private JPanel panTimetable;
+
+        public TimetableFrame(){
+            super("FIM Rozvrhy");
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            initGui();
+        }
+        private void initGui(){
+            timetable = provider.read("J", "22");
+            tabTimetable = new JTable(new TimetableModel());
+
+
+           /* panTimetable = new JPanel();
+
+            JComboBox<String> comboBuilding = new JComboBox<>();
+            JComboBox<String> comboRoom = new JComboBox<>();
+            JButton btnSubmit = new JButton("Hledat");
+
+            panTimetable.add(new JLabel("Budova"));
+            panTimetable.add(comboBuilding);
+            panTimetable.add(new JLabel("Místnost"));
+            panTimetable.add(comboRoom);
+            panTimetable.add(btnSubmit);
+
+            add(panTimetable, BorderLayout.NORTH);
+
+*/
+
+            add(new JScrollPane(tabTimetable), BorderLayout.CENTER);
+
+
+
+
+            pack();
+        }
+
+
+
+
+        class TimetableModel extends AbstractTableModel{
+
+            @Override
+            public String getColumnName(int column) {
+                switch (column){
+
+                    case 0: return "Zkratka";
+                    case 1: return "Název";
+                    case 2: return "Den";
+                    case 3: return "Začátek";
+                    case 4: return "Konec";
+                    case 5: return "Učitel";
+
+                }
+                return super.getColumnName(column);
+            }
+
+            @Override
+            public int getRowCount() {
+                return timetable.getActivities().size();
+            }
+
+            @Override
+            public int getColumnCount() {
+                return 6;
+            }
+
+            @Override
+            public Object getValueAt(int rowIndex, int columnIndex) {
+
+                var act = timetable.getActivities().get(rowIndex);
+
+                switch (columnIndex){
+
+                    case 0: return act.getCode();
+                    case 1: return act.getName();
+                    case 2: return act.getDay();
+                    case 3: return act.getStartTime();
+                    case 4: return act.getEndTime();
+                    case 5: return act.getTeacher();
+
+                }
+
+                return null;
+            }
+        }
+}
